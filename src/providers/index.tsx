a53fd478b76f8176ad77dbbf7048895d8af0dfe3
Iyimide Adekile <adekileiyimide@gmail.com>
2023-07-31 16:11:52 +0100

    First things first

---
+++ src/providers/index.tsx
@@ -0,0 +1,7 @@
+// app/providers.tsx
+
+import { ChakraProvider } from '@chakra-ui/react';
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+	return <ChakraProvider>{children}</ChakraProvider>;
+}
