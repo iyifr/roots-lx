991b62920becbadfeb4f2755a72f43522a27eeef
Iyimide Adekile <adekileiyimide@gmail.com>
2023-07-31 16:40:15 +0100

    (setup): lib-(react-router-dom) working-(true)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-//  import { useState } from 'react'
-import './App.css'
+import { Box } from "@chakra-ui/react"
 
 function App() {
 
   return (
     <>
+    <Box color={"gray.300"} bgColor={"pink.300"}>
+      Hello box
+    </Box>
       Helllo
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import Provider from './providers/index.tsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<Provider>
-			<App />
+			 <RouterProvider router={router} />
+			{/* <App /> */}
 		</Provider>
 	</React.StrictMode>
 );
